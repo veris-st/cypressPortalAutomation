23d4791e4b018e35ee9c2e7acdc8f1a95e123f4b
Shailendra Kumar Tiranga <shailendra.tiranga@veris.in>
2023-10-31 14:52:23 +0530

    Update regressionTesting.js

--- cypress/e2e/regressionTesting.js
+++ cypress/e2e/regressionTesting.js
@@ -385,5 +385,5 @@ cy.wait(5000);
   //it('Logout Test', () => {
 
 
-//This is used for CICD testing use only testing use only
+//This is used for CICD testing use only testing use only. jgjg
   })
